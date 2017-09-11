e92557981125a3ca3f125931901854ed3de48306
Jaison Saji <jaisonsaji@gmail.com>
2017-09-11 14:24:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def upload():
 			for node_id in top_k:
 				human_string = label_lines[node_id]
 				score = predictions[0][node_id]
-				result += str('%s \n' % (human_string))
+				result += str('%s, \n' % (human_string))
 		return result
 
 # This route is expecting a parameter containing the name

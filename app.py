80efec8bf860d939ed2602c6f61d3e3463ecb1d0
Jaison <jaisonsaji@gmail.com>
2017-11-01 12:08:15 +0530

    added LCH graph and gui

--- app.py
+++ app.py
@@ -3,6 +3,38 @@ import sys
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
 import tensorflow as tf
+from nltk.corpus import wordnet as wn
+from nltk.corpus import genesis
+from nltk.corpus import wordnet_ic
+
+from textblob import Word
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+'''def wordnet_graph(w,filename):
+	G=nx.Graph()
+
+	for i in w:
+		for j in w:
+			if j != i:
+				G.add_node(i.name())
+				for h in w.hypernyms():
+					  print (h)
+					  G.add_node(h.name())
+					  G.add_edge(w.name(),h.name())
+
+
+				for h in w.hyponyms():
+					  print (h)
+					  G.add_node(h.name())
+					  G.add_edge(w.name(),h.name())
+
+				#print (G.nodes(data=True))
+				nx.draw(G, width=2, with_labels=True)
+				plt.savefig("uploads/"+filename+".png")
+				plt.clf()
+'''
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -65,14 +97,58 @@ def upload():
 			predictions = sess.run(softmax_tensor, \
 					 {'DecodeJpeg/contents:0': image_data})
 			
+			# Sort to show labels in order of confidence
+			'''top_k = predictions.argsort()[::-1][0:5]
+			for node_id in top_k:
+			  human_string = label_lines[node_id]
+			  synset =  wn._synset_from_pos_and_offset('n',int(human_string))
+			  score = predictions[node_id]
+			  print('%s %s (score = %.5f)' % (human_string, synset,score))'''
+
+		
+
 			# Sort to show labels of first prediction in order of confidence
-			top_k = predictions[0].argsort()[::-1][0:4]
+			top_k = predictions[0].argsort()[::-1][0:5]
+			result=[]
+			labels=[]
+			synset=[]
 			for node_id in top_k:
 				human_string = label_lines[node_id]
 				score = predictions[0][node_id]
-				if human_string != 'pagoda':
-					result += str('%s, \n' % (human_string))
-		return result
+				s = wn._synset_from_pos_and_offset('n',int(human_string))
+				synset.append(s)
+				result.append([human_string,s,score])	
+			print(synset)
+			wn_ic = wn.ic(wn)
+			lch_list=[]
+			val_map={}
+			G =nx.Graph()
+			for i in result:
+				word_i = wn._synset_from_pos_and_offset('n',int(i[0]))
+				for j in result:
+					if i[0] != j[0]:
+						word_j = wn._synset_from_pos_and_offset('n',int(j[0]))
+						lch = word_i.lowest_common_hypernyms(word_j)
+
+						if lch not in lch_list:
+							val_map[lch[0].name()]=0.5714285714285714
+							G.add_node(lch[0].name())
+							lch_list.append(lch)
+
+						if word_i.name() not in G:
+							G.add_node(word_i.name())
+						if word_j.name() not in G:
+							G.add_node(word_j.name())
+							G.add_edge(word_j.name(),lch[0].name())
+						G.add_edge(word_i.name(),lch[0].name())
+						
+			values = [val_map.get(node, 0.15) for node in G.nodes()]
+			print(lch_list)
+			nx.draw(G, width=2, with_labels=True,cmap=plt.get_cmap('jet'),alpha=0.9,node_color=values,font_size=12,font_weight='bold',font_color='#ff4949')
+			plt.savefig("uploads/result.png")
+			plt.clf()
+
+		return render_template('result.html',result=result,path=image_path)
 
 # This route is expecting a parameter containing the name
 # of a file. Then it will locate that file on the upload
